93b7b2616300f8a8b6105a4cd9eb48fc252a3bb8
flmngwllm <flmngwllm232@gmail.com>
2026-03-30 02:53:54 -0400

    Add SNS topic and subscription configuration for self-heal updates

---
+++ terraform/sns.tf
@@ -0,0 +1,9 @@
+resource "aws_sns_topic" "self_heal_sns_topic" {
+  name = "self-heal-updates-topic"
+}
+
+resource "aws_sns_topic_subscription" "self_heal_sns_subscription" {
+  topic_arn = aws_sns_topic.self_heal_sns_topic.arn
+  protocol  = "email"
+  endpoint  = "flmngwllm232@example.com"
+}
