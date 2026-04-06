fd700b7620fd26c109e9febac80198f2707a9c86
flmngwllm <flmngwllm232@gmail.com>
2026-04-05 22:33:35 -0400

    Refactored ECR repository added additional repository to use consistent naming and improve clarity

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,12 +1,26 @@
-resource "aws_ecr_repository" "self_heal_repository" {
-  name                 = "self_heal_repository"
+
+
+resource "aws_ecr_repository" "remediation_serv_repository" {
+  name                 = "self-heal-remediation-serv"
   image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
   }
   tags = {
-    Name = "self_heal_repository"
+    Name = "self-heal-remediation-serv"
+  }
+}
+
+resource "aws_ecr_repository" "app_repository" {
+  name                 = "self-heal-app"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = {
+    Name = "self-heal-app"
   }
 }
 
