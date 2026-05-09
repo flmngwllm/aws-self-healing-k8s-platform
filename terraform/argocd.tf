7e010e5331bac3cd051456e5e63f4ec599beffae
flmngwllm <flmngwllm232@gmail.com>
2026-05-09 19:59:32 -0400

    fmt

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -1,8 +1,8 @@
 resource "helm_release" "self_heal_argocd" {
-  name       = "self-heal-argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  namespace  = "argocd"
+  name             = "self-heal-argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
   create_namespace = true
 
   set = [
