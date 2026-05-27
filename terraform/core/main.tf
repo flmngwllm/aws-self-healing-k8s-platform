793d7879607a284cac56e19e0e0a482a54629584
flmngwllm <flmngwllm232@gmail.com>
2026-05-27 13:54:14 -0400

    added tokens back for pipeline

--- terraform/core/main.tf
+++ terraform/core/main.tf
@@ -4,7 +4,18 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0"
+    }
   }
+
   backend "s3" {
     bucket         = "self-heal-terraform-state"
     key            = "self-heal-platform/terraform.tfstate"
@@ -18,24 +29,47 @@ provider "aws" {
   region = var.REGION
 }
 
-
 data "aws_eks_cluster" "self_heal_cluster" {
-  name = aws_eks_cluster.self_heal_cluster.name
-}
-
-data "aws_eks_cluster_auth" "self_heal_cluster" {
   name = aws_eks_cluster.self_heal_cluster.name
 }
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.self_heal_cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.self_heal_cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.self_heal_cluster.token
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      aws_eks_cluster.self_heal_cluster.name,
+      "--region",
+      var.REGION
+    ]
+  }
 }
 
 provider "helm" {
   kubernetes = {
-    config_path = "~/.kube/config"
+    host                   = data.aws_eks_cluster.self_heal_cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.self_heal_cluster.certificate_authority[0].data)
+
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        aws_eks_cluster.self_heal_cluster.name,
+        "--region",
+        var.REGION
+      ]
+    }
   }
 }
 
